00e089c42333143df113c466f71f4a962941a656
BenM61 <benmassarano@mail.tau.ac.il>
2021-12-28 23:37:20 +0200

    first commit

---
+++ SRC/APPLICATION-SOURCE-CODE/create_table_queries/movies.sql
@@ -0,0 +1,15 @@
+CREATE  TABLE DbMysql08.movies(
+  `movie_id` INT NOT NULL ,
+  `title` CHAR(209) NOT NULL ,
+  `budget` INT UNSIGNED NOT NULL ,
+  `revenue` BIGINT UNSIGNED NOT NULL ,
+  `runtime` MEDIUMINT NOT NULL ,
+  `language` TINYTEXT NOT NULL ,
+  `poster_link` TEXT NULL ,
+  `release_year` YEAR NOT NULL ,
+  `overview` MEDIUMTEXT NOT NULL ,
+  PRIMARY KEY (`movie_id`) ,
+  UNIQUE INDEX `movie_id_UNIQUE` (`movie_id` ASC) ,
+  UNIQUE INDEX `title_UNIQUE` (`title` ASC) ,
+  INDEX `title_index` (`title` ASC) ,
+  INDEX `budget_index` (`budget` ASC) );
